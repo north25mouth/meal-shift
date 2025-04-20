f2bf7bb3bde5f9f9c724fba53d5dacaccd275330
north25mouth <north8mouth@gmail.com>
2025-04-20 13:59:34 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+
+// Firebase設定
+export const firebaseConfig = {
+    apiKey: "AIzaSyAZVrNpZZ0tcKdCk6ICTyysK2v2T9_gOY4",
+    authDomain: "mealshift-32f84.firebaseapp.com",
+    databaseURL: "https://mealshift-32f84-default-rtdb.firebaseio.com",
+    projectId: "mealshift-32f84",
+    storageBucket: "mealshift-32f84.firebasestorage.app",
+    messagingSenderId: "378628974920",
+    appId: "1:378628974920:web:f2872d86eaff8d1a2c2b7d",
+    measurementId: "G-31RL7V07PC"
+};
